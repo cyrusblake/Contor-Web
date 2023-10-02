48b83336606985ee3b97467e1533d5ddf3ccc898
Cyrus Blake <cyrusblake89@gmail.com>
2023-10-02 19:14:33 -0400

    more changes

--- my-app/src/components/ProfileBody.jsx
+++ my-app/src/components/ProfileBody.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import '../styles/ProfileBody.css'
+import Youtube from './Youtube'
 
 
 function ProfileBody(){
     return(
         <>
             <div className="profilebody">
-           
                 <div className="contents">
                     <h1>Profile Body</h1>
-
-                    
-
+                    <Youtube/>
                 </div>
-
-
             </div>
         </>
     )
---
+++ my-app/src/components/Youtube.jsx
@@ -0,0 +1,54 @@
+
+// channel ID = UCpMRD3Q64R97DpPrgEFRRVA
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../styles/Youtube.css'
+
+function Youtube() {
+  const [videos, setVideos] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState('');
+
+  useEffect(() => {
+    // Fetch the initial set of videos
+    fetchVideos();
+  }, []);
+
+  const fetchVideos = () => {
+    const apiKey = 'AIzaSyDtpMOy3zDcfm2vm4C0F7oDREERBcVJ200';
+    const channelId = 'UCpMRD3Q64R97DpPrgEFRRVA';
+    const maxResults = 4;
+
+    axios.get(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&maxResults=${maxResults}&pageToken=${nextPageToken}`)
+      .then(response => {
+        const newVideos = [...videos, ...response.data.items];
+        setVideos(newVideos);
+        setNextPageToken(response.data.nextPageToken);
+      })
+      .catch(error => {
+        console.error('Error fetching data from YouTube:', error);
+      });
+  };
+
+  return (
+    <div className='vid'>
+      {videos.map(video => (
+        <div key={video.id.videoId}>
+          
+          <img
+            src={video.snippet.thumbnails.medium.url}
+            alt={video.snippet.title}
+            height={'100'}
+            width={'200'}
+          />
+          <h5 className="b">{video.snippet.title}</h5>
+        </div>
+      ))}
+      {nextPageToken && (
+        <button onClick={fetchVideos}>Load More</button>
+      )}
+    </div>
+  );
+}
+
+export default Youtube;
