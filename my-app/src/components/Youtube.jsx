01d18a10a57812805c94f1132da0f7d0f7dd1131
Cyrus Blake <cyrusblake89@gmail.com>
2023-11-08 14:42:21 -0500

    more changes

--- my-app/src/components/Youtube.jsx
+++ my-app/src/components/Youtube.jsx
@@ -14,7 +14,6 @@ function Youtube(){
   const fetchVideos = () => {
     const apiKey = 'AIzaSyBHKb5g-GxqYIZyX01A8CQxHgPPYaLSGCM'; // Store the API key securely
     const channelId = '';
-    const maxResults = 6;
     const maxResults = 4;
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&maxResults=${maxResults}&pageToken=${nextPageToken}`;
 
