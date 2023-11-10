42c715a715fda1bc1bb5b1c379a30ac74660f92c
Cyrus Blake <61370086+cyrusblake@users.noreply.github.com>
2023-11-09 20:59:01 -0500

    Update Youtube.jsx

--- my-app/src/components/Youtube.jsx
+++ my-app/src/components/Youtube.jsx
@@ -14,7 +14,6 @@ function Youtube(){
   const fetchVideos = () => {
     const apiKey = 'AIzaSyBHKb5g-GxqYIZyX01A8CQxHgPPYaLSGCM'; // Store the API key securely
     const channelId = '';
-    const maxResults = 4;
     const maxResults = 7;
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&maxResults=${maxResults}&pageToken=${nextPageToken}`;
 
