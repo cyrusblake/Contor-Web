e27af521dd5447f8ca1b45e55c6be087946d77f7
Cyrus Blake <cyrusblake89@gmail.com>
2023-11-10 23:34:17 -0500

    more chnages

--- my-app/src/components/ProfileBody.jsx
+++ my-app/src/components/ProfileBody.jsx
@@ -28,70 +28,63 @@ function ProfileBody(){
 
     return(
         <div className="profilebody">
-                <div className='banner'></div>
-                <div className="contents">
-                    <div className='profile'>
-                        <div className="profile__topLeft">
-                            <img src="https://th.bing.com/th/id/R.fa0ca630a6a3de8e33e03a009e406acd?rik=UOMXfynJ2FEiVw&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2ff%2fa%2f0%2fc%2f1434020125875430376profile.png&ehk=73x7A%2fh2HgYZLT1q7b6vWMXl86IjYeDhub59EZ8hF14%3d&risl=&pid=ImgRaw&r=0" alt=""/>
-                            <div className="profile__TLD">
-                                <h1>name</h1>
-                                <h3>0k followers</h3>
+            <div className='banner'></div>
+            <div className="contents">
+                <div className='profile'>
+                    <div className="profile__topLeft">
+                        <img src="https://th.bing.com/th/id/R.fa0ca630a6a3de8e33e03a009e406acd?rik=UOMXfynJ2FEiVw&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2ff%2fa%2f0%2fc%2f1434020125875430376profile.png&ehk=73x7A%2fh2HgYZLT1q7b6vWMXl86IjYeDhub59EZ8hF14%3d&risl=&pid=ImgRaw&r=0" alt=""/>
+                        <div className="profile__TLD">
+                            <h1>name</h1>
+                            <h3>0k followers</h3>
+                        </div>
+                    </div> 
+                </div>  
+
+                <div className="container">
+                    <div className="tab_box">
+                        <button className="tab_btn active">Home</button>
+                        <button className="tab_btn">Content</button>
+                        <button className="tab_btn">Community</button>
+                        <button className="tab_btn">About</button>
+                        <div className="line"></div>
+                    </div>
+                    <div className="content_box">
+                        <div className="content active">
+                            <div className='c-section'>
+                                <h2>YouTube</h2>
+                                <Youtube/>
+                            </div>
+                            <div className='c-section'>
+                                <h2>Kick</h2>
+                                <div className='kick-section'>
+                                    <Kick/>
+                                    <Kick/>
+                                    <Kick/>
+                                    <Kick/>
+                                    <Kick/>
+                                </div>
+                            </div>
+                            <div className='c-section'>
+                                <h2>Instagram</h2>
+                                <InstagramPost/>
                             </div>
                         </div> 
-                    </div>  
-
-                    <div className="container">
-                        <div className="tab_box">
-                            <button className="tab_btn active">Home</button>
-                            <button className="tab_btn">Content</button>
-                            <button className="tab_btn">Community</button>
-                            <button className="tab_btn">About</button>
-                            <div className="line"></div>
+                        <div className="content">
+                            <Youtube/>
+                            <h2>Content</h2>
+                            <p>ndbcbhbvfbvbvbvbvbbvbvwb</p>
                         </div>
-                        <div className="content_box">
-                            <div className="content active">
-                                <div className='c-section'>
-                                    <h2>YouTube</h2>
-                                    <Youtube/>
-                                </div>
-                                <div className='c-section'>
-                                    <h2>Kick</h2>
-                                    <div className='kick-section'>
-                                        <Kick/>
-                                        <Kick/>
-                                        <Kick/>
-                                        <Kick/>
-                                        <Kick/>
-                                    </div>
-                                </div>
-                                <div className='c-section'>
-                                    <h2>Instagram</h2>
-                                    <InstagramPost/>
-                                </div>
-                            </div> 
-                            <div className="content">
-                                <Youtube/>
-                                <h2>Content</h2>
-                                <p>ndbcbhbvfbvbvbvbvbbvbvwb</p>
-                            </div>
-                            <div className="content">
-                                <h2>Community</h2>
-                                <p>ndbcbhbvfbvbvbvbvbbvbvwb</p>
-                            </div>
-                            <div className="content">
-                                <h2>About</h2>
-                                <p>ndbcbhbvfbvbvbvbvbbvbvwb</p>
-                            </div>
-  
-                           
+                        <div className="content">
+                            <h2>Community</h2>
+                            <p>ndbcbhbvfbvbvbvbvbbvbvwb</p>
+                        </div>
+                        <div className="content">
+                            <h2>About</h2>
+                            <p>ndbcbhbvfbvbvbvbvbbvbvwb</p>
+                        </div>
                     </div>
-
-
-                    </div>  
-                    
-                </div>    
-
-            </div>
-          
+                </div>  
+            </div>    
+        </div>     
     )
 } export default ProfileBody
